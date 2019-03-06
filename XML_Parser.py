9a589699c2951e07b5d0bda067adf0ca0479eae6
arasmuss <arasm002@.odu.edu>
2019-03-05 22:36:18 -0600

    Modify JSON formatting to pretty print

--- XML_Parser.py
+++ XML_Parser.py
@@ -2,6 +2,29 @@ from utilities import *
 import pandas as pd
 
 
+
+
+def elem_to_dictionary(elem):
+    d=dict(tag=elem.tag)
+    if elem.text:
+        d['text']=elem.text
+    if elem.attrib:
+        d['attributes']=elem.attrib
+    children=elem.getchildren()
+    if children:
+        d['children']=map(elem_to_dictionary, children)
+    if elem.tail:
+        d['tail']=elem.tail
+    return d
+
+                      
+def to_json(elem):
+
+    if hasattr(elem, 'getroot'):
+        elem=elem.getroot()
+    return simplejson.dumps(elem_to_dictionary(elem), indent=4 * ' ')
+
+
 def depth(node):
     d = 0
     while node is not None:
@@ -19,46 +42,13 @@ def get_max_depth():
     max_depth = max(levels)
     return max_depth
 
-def elem_to_pesterfish(elem):
-    """
-    turns an elementtree-compatible object into a pesterfish dictionary
-    (not json).
-    
-    """
-    d=dict(tag=elem.tag)
-    if elem.text:
-        d['text']=elem.text
-    if elem.attrib:
-        d['attributes']=elem.attrib
-    children=elem.getchildren()
-    if children:
-        d['children']=map(elem_to_pesterfish, children)
-    if elem.tail:
-        d['tail']=elem.tail
-    return d
-
-                  
-def to_pesterfish(elem):
-    """
-    turns an elementtree-compatible element or tree
-    into a pesterfish json string.
-    
-    """
-    if hasattr(elem, 'getroot'):
-        elem=elem.getroot()
-    return simplejson.dumps(elem_to_pesterfish(elem))
-
-
-def main():
-
-    print(to_pesterfish(tree))
-
+def to_xml_tree():
     for element in tree.getiterator():
 
-   
+
         if element.text.strip() == "":
             print("Parent: " + str(element.tag))
-    
+
         else:
             print("\tChild: " + str(element.tag) + " - " + repr(element.text.strip()))
 
@@ -66,47 +56,15 @@ def main():
     print("-" * 20)
     print("Max depth is: " + str(get_max_depth()))
 
-'''
-    NESTED_DEPTH_CHILD = 0
 
 
-    print("Root tag: " + str(root.tag))
-    print("Root attrib: " + str(root.attrib))
-    print("Root text: " + repr(root.text))
+def main():
 
-    for child in root:
-        #print("Child tag: " + str(child.tag))
-        #print("Child attrib: " + str(child.attrib))
-        #print("Child text: " + repr(child.text))
-
-        listOfChildTags = [x.tag for x in root.findall(child.tag+"/*")]
+    print(to_json(tree))
+    to_xml_tree()
 
 
-        NESTED_DEPTH_CHILD += 1
 
-    print(listOfChildTags[0:])
-
-
-    getUniqueSet = {}
-
-    for elem in listOfChildTags:
-        getUniqueSet.insert(elem)
-
-    print(getUniqueSet)
-'''
-'''
-    print("\t\t\tNested Depth: " + str(NESTED_DEPTH_CHILD))
-
-    if NESTED_DEPTH_CHILD == 1:
-
-        for child in root:
-            for sub_child in child:
-                print("Subchild tag: " + str(sub_child.tag))
-                #print("Subchild attrib: " + str(sub_child.attrib))
-                #print("Subchild text: " + repr(sub_child.text))
-                NESTED_DEPTH_SUB_CHILD += 1
-
-'''
 
 if __name__ == "__main__":
     main()
